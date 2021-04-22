c217725f6a5a56a8f2246e3e79b9ce55b31d4213
MacBook <macbook@MacBook-Pro-de-MacBook.local>
2021-04-22 10:11:36 -0300

    new dashboard

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import PaymentCard from 'react-payment-card-component'
-import MiniCard from  './MiniCard';
-import LargeCard from './LargeCard';
-
+import logo from "./logo.svg";
+import "./App.css";
+import PaymentCard from "react-payment-card-component";
+import MiniCard from "./MiniCard";
+import LargeCard from "./LargeCard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <div className="card">
-         <PaymentCard />
-      </div> 
-        
-    {/* <MiniCard />
+        <div className="card">
+        <MiniCard />
+        <LargeCard />
+          <div className="new-payment">
+            <PaymentCard />
+          </div>
+         
+          
+          
+        </div>
+
+        {/* <MiniCard />
     <LargeCard />   */}
-    <div className="card-pay">    
-      {/* <PaymentCard
+        <div className="card-pay">
+          {/* <PaymentCard
       bank="itau"
       model="personnalite"
       type="black"
@@ -27,8 +33,8 @@ function App() {
       expiration="12/20"
       flipped={false}
     /> */}
-    </div>
-  </header>
+        </div>
+      </header>
     </div>
   );
 }
